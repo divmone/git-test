a107251000f2c5f70d66f62f694aa14da71c98be
divmone <dimasston99@gmail.com>
2022-01-05 17:23:12 +0300

    first commit

---
+++ neonic.ino
@@ -0,0 +1,246 @@
+
+
+#define SW 4      // кнопка подключена сюда (PIN --- КНОПКА --- GND)
+#define MODE_AM 6   // количество режимов (от 0 до указанного)
+#define LEDS 5
+#define CLK 2  //s1
+#define DT 3 //s2
+#include <EEPROM.h>
+#include <GyverEncoder.h>
+#include <GyverTimers.h>
+#include "FastIO.h"
+
+
+
+volatile int colors[LEDS];
+uint8_t mode = 0;       // переменная режима
+int ledDelay = 500;
+int direction = 1;
+int currentLed = 0;
+unsigned long changeTime;
+int i = 0;
+bool flag;
+byte ledBrigth = 10;
+const uint8_t pins[] = {5, 6, 9, 10, 11};
+boolean eepromFlag = false;
+uint32_t eepromTimer = 0;
+
+
+Encoder enc1(CLK, DT, SW);
+
+void setup() {
+  Serial.begin(9600);
+  for (int i = 0; i < 13; i++) {
+    pinMode(pins[i], OUTPUT);
+  }
+  changeTime = millis();
+  enc1.setType(TYPE2);
+  Timer2.setFrequency(40000);
+  Timer2.enableISR();
+  EEPROM.get(0, mode);
+  EEPROM.get(50, ledDelay);
+}
+
+void enc() {
+  enc1.tick(); // отработка в прерывании
+
+  if (enc1.isLeft())  {
+    ledDelay = ledDelay - 50;
+    if (ledDelay == 0) ledDelay = 50;
+    eepromFlag = true;
+    eepromTimer = millis();
+  }
+  if (enc1.isRight()) {
+    ledDelay = ledDelay + 50;
+    eepromFlag = true;
+    eepromTimer = millis();
+  }
+  if (enc1.isClick()) {
+    if (++mode >= MODE_AM) mode = 0;
+    for (uint8_t i = 0; i < LEDS; i++) colors[i] = 0;
+    EEPROM.put(0, mode);
+
+  }
+  if(enc1.isLeftH())  {
+ledBrigth = ledBrigth - 10; 
+if(ledBrigth = 0) ledBrigth = 10;
+  }
+  if (enc1.isRightH()){
+    ledBrigth = ledBrigth + 10; 
+    if(ledBrigth = 250) ledBrigth = 250;
+  
+    
+    }
+}
+
+
+void loop() {
+  
+  checkEEPROM();
+  enc();
+  static uint32_t timer = millis();
+  if (millis() - timer >= ledDelay / 5) {
+    timer = millis();
+    switch (mode) {
+      case 3: task_3();
+        break;
+      case 4: task_4();
+        break;
+      case 5: task_5();
+        break;
+    }
+  }
+  switch (mode) {
+    case 0: task_0();
+      break;
+    case 1: task_1();
+      break;
+    case 2: task_2();
+      break;
+
+  }
+}
+
+
+
+
+void changeLed() {
+  for (int i = 0; i < LEDS; i++) {
+    digitalWrite(pins[i], LOW);
+  }
+  digitalWrite(pins[currentLed], 1);
+  currentLed += direction;
+  if (currentLed == LEDS-1 ) {
+    direction = -1;
+  }
+  if (currentLed == 0) {
+    direction = 1;
+  }
+}
+
+
+
+void changeLed2() {
+  for (int i = 0; i < LEDS; i++) {
+    digitalWrite(pins[i], LOW);
+  }
+  digitalWrite(pins[currentLed], HIGH);
+  currentLed += direction;
+  if (currentLed == LEDS) {
+    currentLed = 0;
+  }
+  if (currentLed == 0) {
+    direction = 1;
+  }
+}
+void changeLed3() {
+  static byte i = 0;
+  digitalWrite(pins[i], 1);
+  i++;
+  if (i > LEDS) {
+    for (int i = 0; i < LEDS; i++) {
+      digitalWrite(pins[i], LOW);
+    }
+    i=0;
+  }
+}
+
+
+
+int fadeAmount = 5;
+int bright = 0;
+void fade() {
+  bright = bright + fadeAmount;
+  if (bright <= 0 || bright >= 255) {
+    fadeAmount = -fadeAmount;
+  }
+  for (int i = 0; i < LEDS; i++) {
+    colors[i] = bright;
+  }
+}
+
+
+
+void confetti() {
+  for (int i = 0; i < LEDS; i++) {
+    if (colors[i] == 0 && ((int)random(0, 15) == 0) ) {
+      colors[i] = 255;
+    }
+    colors[i] = max(colors[i] -= 10, 0);
+  }
+}
+
+int trackPos = 0;
+int trackVel = 4;
+void track() {
+  trackPos += trackVel;
+  if (trackPos < 0 || trackPos >= LEDS * 10) trackVel = -trackVel;
+  colors[constrain(trackPos / 10, 0, LEDS - 1)] = 255;
+  for (int i = 0; i < LEDS; i++) {
+    colors[i] = max(colors[i] -= 15, 0);
+  }
+}
+
+void task_0() {
+  Serial.println("Task 0");
+  if ((millis() - changeTime) > ledDelay) {
+    changeLed();
+    changeTime = millis();
+  }
+}
+void task_1() {
+  Serial.println("Task 1");
+  if ((millis() - changeTime) > ledDelay) {
+    changeLed2();
+    changeTime = millis();
+  }
+}
+void task_2() {
+  Serial.println("Task 2");
+  if ((millis() - changeTime) > ledDelay) {
+    changeLed3();
+    changeTime = millis();
+  }
+}
+
+void task_3() {
+  fade();
+}
+
+void task_4() {
+  confetti();
+}
+
+void task_5() {
+  track();
+}
+
+
+void pwmTick() {
+  static volatile uint8_t counter = 0;
+  if (counter == 0) {
+    for (uint8_t i = 0; i < LEDS; i++) {
+      if (colors[i] > 0) fastWrite(pins[i], HIGH);
+    }
+  }
+  for (uint8_t i = 0; i < LEDS; i++) {
+    if (counter == colors[i] && mode > 2 ) {
+      fastWrite(pins[i], LOW);
+    }
+  }
+  counter++;
+}
+
+ISR(TIMER2_A) {
+  pwmTick();
+}
+
+
+
+void checkEEPROM() {
+  // если флаг поднят и с последнего нажатия прошло 10 секунд (10 000 мс)
+  if (eepromFlag && (millis() - eepromTimer >= 10000) ) {
+    eepromFlag = false;           // опустили флаг
+    EEPROM.put(1, ledDelay);     // записали в EEPROM
+  }
+}
